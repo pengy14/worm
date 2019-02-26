4dd01dce9dbb2844b904a4ca9ee6ede0829cc248
pengy <1013799318@qq.com>
2019-02-26 17:38:37 +0800

    v1 china

--- mining.py
+++ mining.py
@@ -3,7 +3,7 @@ import json
 from openpyxl import Workbook
 from bs4 import BeautifulSoup
 from fake_useragent import UserAgent
-import urllib.request
+
 
 wb = Workbook()
 ws = wb.active
@@ -15,65 +15,75 @@ def request():
     return json.loads(resp.text).get('features')
 
 
-def getCoal(url,rowdata):
+def getCoal(url, rowdata):
     # headers ={"User-Agent: Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.109 Mobile Safari/537.36"}
-    req = urllib.request.Request(
-        url,
-        data=None,
-        headers={
-            'User-Agent': UserAgent().random
-        }
-    )
-    res = urllib.request.urlopen(req)
-    html = res.read()
-    soup = BeautifulSoup(html, 'html.parser')
-    result = rowdata
-    Coal = soup.find("b", string="Type:")
-    if Coal is None:
-        result.append("-1")
-    else:
-        Coalli = Coal.find_parent("li")
-        if Coalli is None:
+    headers = {'User-Agent': UserAgent().random}
+    # print(url)
+    try:
+        res = requests.get(url, headers=headers, verify=False)
+        html = res.text
+        soup = BeautifulSoup(html, 'html.parser')
+        result = rowdata
+        Coal = soup.find("b", string="Type:")
+        if Coal is None:
             result.append("-1")
         else:
-            type = Coalli.get_text(' ', strip=True)
-            typeArr = type.split()
-            if (len(typeArr) == 2):
-                result.append(typeArr[1])
-            else:
+            Coalli = Coal.find_parent("li")
+            if Coalli is None:
                 result.append("-1")
-    Coal_type = soup.find("b", string="Coal type:")
-    if Coal_type is None:
-        result.append("-1")
-    else:
-        Coal_type_li = Coal_type.find_parent("li")
-        if Coal_type_li is None:
+            else:
+                type = Coalli.get_text(' ', strip=True)
+                typeArr = type.split()
+                if (len(typeArr) == 2):
+                    result.append(typeArr[1])
+                else:
+                    result.append("-1")
+        Coal_type = soup.find("b", string="Coal type:")
+        if Coal_type is None:
             result.append("-1")
         else:
-            coaltype = Coal_type_li.get_text(' ', strip=True)
-            coaltypeArr = coaltype.split()
-            if len(coaltypeArr) == 3:
-                result.append(coaltypeArr[2])
-            else:
+            Coal_type_li = Coal_type.find_parent("li")
+            if Coal_type_li is None:
                 result.append("-1")
-    return result
+            else:
+                coaltype = Coal_type_li.get_text(' ', strip=True)
+                coaltypeArr = coaltype.split()
+                if len(coaltypeArr) == 3:
+                    result.append(coaltypeArr[2])
+                else:
+                    result.append("-1")
+        return result
+    except requests.exceptions.RequestException as e:
+        print(e)
+        pass
 
 
 def toExcel(title, features, excelfile):
     ws.append(title)
+    # i = 0
     for eachfeature in features:
         rowdata = []
         properties = eachfeature.get('properties')
         status = properties.get('status')
-        if (status == 'shelved') or (status == 'cancelled'):
+        if properties.get('country') == 'China' and ((status == 'shelved') or (status == 'cancelled')):
             wiki_page = properties.get('wiki_page')
             geometry = eachfeature.get('geometry')
             for eachtitle in title:
+                if eachtitle == 'coordinates':
+                    break
                 rowdata.append(properties.get(eachtitle))
             coordinates = str(geometry.get('coordinates')[0]) + ',' + str(geometry.get('coordinates')[1])
             rowdata.append(coordinates)
-            rowdata = getCoal(wiki_page,rowdata)
-            ws.append(rowdata)
+            # i = i + 1
+            result = getCoal(wiki_page, rowdata)
+            if result is None:
+                rowdata.append('-1')
+                rowdata.append('-1')
+                ws.append(rowdata)
+            else:
+                ws.append(result)
+        # if i == 20:
+        #     break
     print('saving')
     wb.save(excelfile)
 
@@ -87,3 +97,13 @@ if __name__ == '__main__':
              "subnational_unit", "annual_co2_mtons", "coordinates", "Coal", "Coal type"]
     features = request()
     toExcel(title, features, './coalmining.xlsx')
+    # c = ["00","11"]
+    # resu =getCoal("http://bit.ly/1NV0AUY",title)
+    # if resu is None:
+    #     title.append('-2')
+    #     title.append('-2')
+    #     for s in title:
+    #         print(s)
+    # else:
+    #     for s in resu:
+    #         print(s)
